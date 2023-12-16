6d8df6d86576b7602dad7f63d0ffc2aab05f3bc7
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-16 22:00:53 +0800

    fixed UI

--- components/page/home/component.tsx
+++ components/page/home/component.tsx
@@ -5,28 +5,35 @@ import dayjs from "dayjs";
 
 // UI
 import { DisplayHome } from "@/app/home/display";
-import { CheckCircle2Icon } from "lucide-react";
+import { CheckCircle2Icon, Loader2 } from "lucide-react";
 
 const HomeComponent = () => {
   const savings = useFetchSavingsToday();
-  console.log(savings);
 
   const goal = (savings.data?.goal || 0) - (savings.data?.savedAmount || 0);
 
   return (
     <div className="flex-1 flex justify-center items-center flex-col gap-6 text-main-600">
-      {savings?.data?.metGoal ? (
-        <div className="flex-1 flex justify-center items-center flex-col gap-4 text-center">
-          <CheckCircle2Icon className="w-32 h-32 text-green-500" />
-          <p className="text-6xl font-bold">Already Paid Today</p>
-          <span className="text-xl">{dayjs().format("MMMM DD")}</span>
-        </div>
+      {savings.isLoading || savings.data === undefined ? (
+        <>
+          <Loader2 className="w-10 h-10 animate-spin" />
+        </>
       ) : (
         <>
-          <DisplayHome
-            goal={goal || 100}
-            savedAmount={savings?.data?.savedAmount || 0}
-          />
+          {savings?.data?.metGoal ? (
+            <div className="flex-1 flex justify-center items-center flex-col gap-4 text-center">
+              <CheckCircle2Icon className="w-32 h-32 text-green-500" />
+              <p className="text-6xl font-bold">Already Paid Today</p>
+              <span className="text-xl">{dayjs().format("MMMM DD")}</span>
+            </div>
+          ) : (
+            <>
+              <DisplayHome
+                goal={goal as number}
+                savedAmount={savings?.data?.savedAmount as number}
+              />
+            </>
+          )}
         </>
       )}
     </div>
